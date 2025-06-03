45da15745a90ed72be26ac5adfec64756b52f182
Deniz Bayrak <154671872+denizbyrk@users.noreply.github.com>
2025-06-04 00:23:19 +0300

    Update User.java

--- src/main/java/com/chatapp/chat_backend/model/User.java
+++ src/main/java/com/chatapp/chat_backend/model/User.java
@@ -9,12 +9,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private String username;
 
     @Column(nullable = false)
     private String password;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
     // ----- Constructor -----
